356d3b5fe7e93317aae527f0e2e757e93963bf79
AntonFreezer <anton.kholodkov@gmail.com>
2024-02-15 14:14:57 +0300

    Initial Commit

---
+++ DayPic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DayPic
+//
+//  Created by Anton Kholodkov on 15.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
